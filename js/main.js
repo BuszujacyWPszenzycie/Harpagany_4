4b5db65cec8073a783b3ef4a3d8eaa504cf11d74
Marcin <marcin.szturminski@gmail.com>
2023-12-03 20:30:44 +0100

    finished page - only for desktop

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ const arrowIcon = document.querySelector('.header__icon')
 const modal = document.querySelector('.modal')
 const allNavItems = document.querySelectorAll('.nav__item')
 
-let images = ['team_1', 'team_2', 'team_3']
+let images = ['team_1', 'team_2', 'team_3', 'team_4', 'team_5', 'team_6']
+
+console.log(images)
 
 const showModal = () => {
 	modal.classList.toggle('show-modal')
@@ -17,20 +19,19 @@ allNavItems.forEach(item =>
 )
 
 let indexChange = 0
-let indexGoLeft = 0
-let indexGoRight = 0
 
 const aboutImg = document.querySelector('.about__img')
 
 const changePicture = () => {
 	aboutImg.src = `./img/${images[indexChange]}.jpg`
-	indexChange > 1 ? (indexChange = 0) : indexChange++
+	indexChange > 4 ? (indexChange = 0) : indexChange++
+	console.log(indexChange)
 }
 
 arrowIcon.addEventListener('click', showModal)
 
 changePicture()
 
-// window.onload = function () {
-// 	setInterval(changePicture, 4000)
-// }
+window.onload = function () {
+	setInterval(changePicture, 4000)
+}
